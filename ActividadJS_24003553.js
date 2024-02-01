b8f565365f9a1d65727dcdf32d6f5c09719bd8d5
Breyner Suarez <breyner.suarez@galileo.edu>
2024-01-31 19:05:23 -0600

    first commit

---
+++ ActividadJS_24003553.js
@@ -0,0 +1,76 @@
+var mes = prompt("Mes de nacimiento")
+var dia = prompt("Día de nacimiento")
+
+if (mes == 3) {
+    if (dia >= 21) {
+        alert("Es aries")
+    } else {
+        alert("Es piscis")
+    }
+} else if (mes == 4) {
+    if (dia <= 20) {
+        alert("Es aries")
+    } else {
+        alert("Es tauro")
+    }
+} else if (mes == 5) {
+    if (dia <= 20) {
+        alert("Es tauro")
+    } else {
+        alert("Es geminis")
+    }
+} else if (mes == 6) {
+    if (dia <= 20) {
+        alert("Es geminis")
+    } else {
+        alert("Es cancer")
+    }
+} else if (mes == 7) {
+    if (dia <= 22) {
+        alert("Es cancer")
+    } else {
+        alert("Es leo")
+    }
+} else if (mes == 8) {
+    if (dia <= 22) {
+        alert("Es leo")
+    } else {
+        alert("Es virgo")
+    }
+} else if (mes == 9) {
+    if (dia <= 22) {
+        alert("Es virgo")
+    } else {
+        alert("Es libra")
+    }
+} else if (mes == 10) {
+    if (dia <= 22) {
+        alert("Es libra")
+    } else {
+        alert("Es escorpio")
+    }
+} else if (mes == 11) {
+    if (dia <= 21) {
+        alert("Es escorpio")
+    } else {
+        alert("Es sagitario")
+    }
+} else if (mes == 12) {
+    if (dia <= 21) {
+        alert("Es sagitario")
+    } else {
+        alert("Es capricornio")
+    }
+} else if (mes == 1) {
+    if (dia <= 19) {
+        alert("Es capricornio")
+    } else {
+        alert("Es acuario")
+    }
+} else if (mes == 2) {
+    if (dia <= 18) {
+        alert("Es acuario")
+    } else {
+        alert("Es piscis")
+    }
+}
